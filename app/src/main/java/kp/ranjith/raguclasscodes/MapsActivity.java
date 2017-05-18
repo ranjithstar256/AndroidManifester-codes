8dfa44984a3393494c2f437dd94697bb8c32fc61
Ranjith <ranjithstar256@gmail.com>
2017-05-18 17:58:48 +0530

    sms mail added

--- app/src/main/java/kp/ranjith/raguclasscodes/MapsActivity.java
+++ app/src/main/java/kp/ranjith/raguclasscodes/MapsActivity.java
@@ -1,5 +1,8 @@
 package kp.ranjith.raguclasscodes;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -25,22 +28,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Example Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
--- app/src/main/java/kp/ranjith/raguclasscodes/SendingSmsMail.java
+++ app/src/main/java/kp/ranjith/raguclasscodes/SendingSmsMail.java
@@ -1,13 +1,51 @@
 package kp.ranjith.raguclasscodes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SendingSmsMail extends AppCompatActivity {
+
+    EditText editText1,editText2,editText3,editText4,editText5;
+    String number,smsmsg,mailid,mailsub,mailmsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sending_sms_mail);
+        editText1= (EditText) findViewById(R.id.editText);
+        editText2= (EditText) findViewById(R.id.editText2);
+        editText3= (EditText) findViewById(R.id.editText7);
+        editText4= (EditText) findViewById(R.id.editText8);
+        editText5= (EditText) findViewById(R.id.editText9);
+    }
+
+    public void sendsms(View view) {
+        number=editText1.getText().toString();
+        smsmsg=editText2.getText().toString();
+        SmsManager smsManager =  SmsManager.getDefault();
+        smsManager.sendTextMessage(number,null,smsmsg,null,null); //we need to add permission in manifest file to send sms
+        Toast.makeText(this, "Sms Send successfully!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void sendmail(View view) {
+        mailid=editText3.getText().toString();
+        mailsub=editText4.getText().toString();
+        mailmsg=editText5.getText().toString();
+
+
+        Intent email = new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_EMAIL, new String[]{ mailid});
+        email.putExtra(Intent.EXTRA_SUBJECT, mailsub);
+        email.putExtra(Intent.EXTRA_TEXT, mailmsg);
+
+        //need this to prompts email client only
+        email.setType("message/rfc822");
+
+        startActivity(Intent.createChooser(email, "Choose an Email client :")); // no permission needed for mail
     }
 }
