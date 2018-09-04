8cd023804dea7f527cc44cf4bccc71ccd3ee7481
Ranjith <ranjithstar256@gmail.com>
2018-09-04 14:36:12 +0530

    added formcodes

--- app/src/main/java/kp/ranjith/raguclasscodes/WifiBluetooth.java
+++ app/src/main/java/kp/ranjith/raguclasscodes/WifiBluetooth.java
@@ -52,6 +52,63 @@ public class WifiBluetooth extends AppCompatActivity {
             }
         });
 
+        
+        
+        <!--Phone dial-->
+        <Entry x:Name="MsgTo" Placeholder="PhoneNumber" HorizontalOptions="FillAndExpand"/>
+        <Entry x:Name="Message" Placeholder="MessageText" HorizontalOptions="FillAndExpand"/>
+        <Button Text="SMS"   Clicked="BtnSend_Click" BackgroundColor="White"/>
+
+        <!--SMS-->
+        <Entry x:Name="PhoneNumber" Placeholder="PhoneNumber" HorizontalOptions="FillAndExpand"/>
+        <Button Text="CALL"  Clicked="BtnCall_Click" BackgroundColor="White"/>
+
+        <!--Email-->
+        <Entry x:Name="EmailTo" Placeholder="EmailTo" HorizontalOptions="FillAndExpand"/>
+        <Entry x:Name="EmailSubject" Placeholder="Subject" HorizontalOptions="FillAndExpand"/>
+        <Entry x:Name="EmailBody" Placeholder="MessageText" HorizontalOptions="FillAndExpand"/>
+        <Button Text="EMAIL" Clicked="BtnEmail_Click" BackgroundColor="White"/>
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+
+            var SmsTask = CrossMessaging.Current.SmsMessenger;
+
+            if (SmsTask.CanSendSms)
+                SmsTask.SendSms(MsgTo.Text, Message.Text);
+        }
+
+
+        private void BtnCall_Click(object sender, EventArgs e)
+        {
+            var PhoneCallTask = CrossMessaging.Current.PhoneDialer;
+            if (PhoneCallTask.CanMakePhoneCall)
+                PhoneCallTask.MakePhoneCall(PhoneNumber.Text);
+        }
+
+
+        private void BtnEmail_Click(object sender, EventArgs e)
+        {
+            var EmailTask = CrossMessaging.Current.EmailMessenger;
+
+            if (EmailTask.CanSendEmail)
+                EmailTask.SendEmail(EmailTo.Text, EmailSubject.Text, EmailBody.Text);
+        }
+
+		
+        
 
     }
 }
