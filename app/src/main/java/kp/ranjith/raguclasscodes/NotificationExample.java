98003b9cc5dff23bebc7377090caaee7102f2bdf
Ranjith <ranjithstar256@gmail.com>
2019-06-13 14:51:13 +0530

    Notification code update

--- app/src/main/java/kp/ranjith/raguclasscodes/NotificationExample.java
+++ app/src/main/java/kp/ranjith/raguclasscodes/NotificationExample.java
@@ -14,8 +14,38 @@ public class NotificationExample extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_example);
-
+showNotification(this, "", "", intent) ;
     }
+
+public void showNotification(Context context, String title, String body, Intent intent) {
+    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+    int notificationId = 1;
+    String channelId = "channel-01";
+    String channelName = "Channel Name";
+    int importance = NotificationManager.IMPORTANCE_HIGH;
+
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        NotificationChannel mChannel = new NotificationChannel(
+                channelId, channelName, importance);
+        notificationManager.createNotificationChannel(mChannel);
+    }
+
+    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(body);
+
+    TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+    stackBuilder.addNextIntent(intent);
+    PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(
+            0,
+            PendingIntent.FLAG_UPDATE_CURRENT
+    );
+    mBuilder.setContentIntent(resultPendingIntent);
+
+    notificationManager.notify(notificationId, mBuilder.build());
+}
 
     public void shownofn(View view) {
 
